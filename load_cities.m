60afa5c79e621d70dc97bea96c63be7b35e9a346
maciek-slon <maciek.slon@gmail.com>
2011-12-12 16:55:15 +0100

    Poprawki wydajnosciowe

--- load_cities.m
+++ load_cities.m
@@ -1,48 +1,53 @@
-function [distances, stink, nr_cities, cities] = load_cities(filename, type='euc', initial_stink = 1)	
+function [distances, stink, nr_cities, cities] = load_cities(filename, type='euc', initial_stink = 1)
 	% load cities from data file
 	cities = load('-ascii', filename);
 	nr_cities = size(cities,1);
-	
+
 	% compute distance array
 	distances = zeros(nr_cities, nr_cities);
-	
+
+	num_loops = nr_cities * (nr_cities-1) / 2;
+	done_loops = 0;
+
 	for x = 2:nr_cities
-		fprintf(2, "%d/%d\r", x, nr_cities);
+		fprintf(2, "Loading cities and computing distances [%2.0f%%]\r", 100 * done_loops / num_loops);
 		for y = 1:x-1
 			x_1 = cities(x, 2);
 			y_1 = cities(x, 3);
 			x_2 = cities(y, 2);
 			y_2 = cities(y, 3);
-			
+
 			dist = 0;
 			if type == 'geo'
 				lat_1 = calculate(x_1);
 				lon_1 = calculate(y_1);
 				lat_2 = calculate(x_2);
 				lon_2 = calculate(y_2);
-				
+
 				RRR = 6378.388;
 				q1 = cos(lon_1 - lon_2);
 				q2 = cos(lat_1 - lat_2);
 				q3 = cos(lat_1 + lat_2);
-				
+
 				dist = round(RRR*acos(0.5 * ((1.0+q1)*q2 - (1.0-q1)*q3)) + 1.0);
 				distances(x,y) = round(dist);
 				distances(y,x) = round(dist);
-			else if type=='euc'
+			elseif type=='euc'
 				dist = sqrt((x_2-x_1)^2 + (y_2-y_1)^2);
 				distances(x,y) = round(dist);
 				distances(y,x) = round(dist);
 			end
-			
+
 		end
+		done_loops = done_loops + x - 1;
 	end
 
 	% initial feromone values
 	if initial_stink < 0
+		fprintf(2, "random stink\n");
 		stink = rand(nr_cities, nr_cities);
 	else
+		fprintf(2, "stink: %d\n", initial_stink);
 		stink = initial_stink * ones(nr_cities, nr_cities);
 	end
 end
-
--- main.m
+++ main.m
@@ -1,23 +1,22 @@
-function [ret_distance, ret_route, best_round, shortest_tab] = main(filename, type='euc', total_ants = 50, stink_fade = 0.95, stink_power = 0.1, total_rounds = 1000, initial_stink = 1, headless = 0) 
-	
-	fprintf(2, "Loading cities and computing distances...\n");
+function [ret_distance, ret_route, best_round, shortest_tab] = main(filename, type='euc', total_ants = 50, stink_fade = 0.95, stink_power = 0.1, total_rounds = 1000, initial_stink = 1, headless = 0)
+
 	[d, s, n, c] = load_cities(filename, type, initial_stink);
-	
+
 	number_of_rounds_in_history_of_world = total_rounds;
 	number_of_the_ants_in_our_universe = total_ants;
-	
+
 	shortest_tab = zeros(1, number_of_rounds_in_history_of_world);
 	longest_tab = zeros(1, number_of_rounds_in_history_of_world);
-	
+
 	last_shortest = 0;
-	
+
 	shortest = 10000000000000000000;
 	counter = 0;
-	
-	
-	
-	
-	
+
+
+
+
+
 	nazwa = sprintf("%s-fade-%.2f-power-%.2f-ants-%d", filename, stink_fade, stink_power, total_ants)
 	tries = 1;
 	nazwa_folderu = nazwa;
@@ -26,71 +25,75 @@ function [ret_distance, ret_route, best_round, shortest_tab] = main(filename, ty
 		nazwa_folderu = sprintf("%s-%d", nazwa, tries);
 		tries = tries + 1;
 	end
-	
+
 	mkdir(nazwa_folderu);
-	
-	
-	
-	
-	
+
+
+
+
+
 	fprintf(2, "%d ants started...\n", number_of_the_ants_in_our_universe);
-	
+
 	total_time = 0;
-		
+
 	clf;
-	
+
 	offset = 0;
-		
+
 	for round=1:number_of_rounds_in_history_of_world
-		
+
 		tic;
-		
+
 		current_dists = zeros(1, number_of_the_ants_in_our_universe);
-		
+
 		ants = prepare_ant(number_of_the_ants_in_our_universe, n);
 		available_cities = repmat(1:n, number_of_the_ants_in_our_universe, 1);
 		ants(:,end) = ants(:,1);
-		
+
+		global_probs = s./d;
+
 		% compute next step for every ant
 		for step=2:n
 			new_available_cities = zeros(number_of_the_ants_in_our_universe, n-step+1);
-		
+
+
 			for ant=1:number_of_the_ants_in_our_universe
 				last_city = ants(ant,step-1);
 				av_cities =	available_cities(ant,:);
 				av_cities(av_cities == last_city) = [];
-				
-				stinks = s(last_city,av_cities);
-				dists = d(last_city,av_cities);
-				probs = cumsum(stinks./dists);
+
+				%stinks = s(last_city,av_cities);
+				%dists = d(last_city,av_cities);
+				%probs = cumsum(stinks./dists);
+				probs = cumsum(global_probs(last_city, av_cities));
 				x = rand * probs(end);
 				index = find(probs>=x, 1);
 				ants(ant,step) = av_cities(index);
 				new_available_cities(ant,:) = av_cities;
-				
+
 				current_dists(ant) = current_dists(ant) + d(last_city, av_cities(index));
 			end
 			available_cities = new_available_cities;
 		end
-		
+
 		local_shortest = 10000000000;
 		local_longest = 0;
 		local_ant = 0;
 		for ant=1:number_of_the_ants_in_our_universe
 			current_dists(ant) = current_dists(ant) + d(ants(ant, end), ants(ant, end-1));
-			
+
 			if current_dists(ant) < local_shortest
 				local_shortest = current_dists(ant);
 				local_ant = ant;
 			end
-			
+
 			if current_dists(ant) > local_longest
 				local_longest = current_dists(ant);
 			end
 		end
-		
+
 		s = s * stink_fade;
-		
+
 		for i=2:n+1
 			c1 = ants(local_ant, i-1);
 			c2 = ants(local_ant, i);
@@ -98,31 +101,31 @@ function [ret_distance, ret_route, best_round, shortest_tab] = main(filename, ty
 			s(c1, c2) = st + stink_power;
 			s(c2, c1) = st + stink_power;
 		end
-		
+
 		if local_shortest < shortest
 			ret_route = ants(local_ant,:);
 			shortest = local_shortest;
 			best_round = round;
 		end
-		
-		
-		
+
+
+
 		if last_shortest == local_shortest
 			count = count + 1;
 		else
 			count = 0;
 		end
-		
+
 		last_shortest = local_shortest;
-		
+
 		total_time = total_time + toc;
 
 		[mm, ss] = calculate_remaining(total_time, round, number_of_rounds_in_history_of_world);
 		fprintf(2, "After round %d/%d. Shortest: %d (%d) %d Time remaining: %d:%02d [~%.2fs/rnd]      \r", round, number_of_rounds_in_history_of_world, shortest, local_shortest, count, mm, ss, total_time/round);
-		
+
 		shortest_tab(round) = local_shortest;
 		longest_tab(round) = local_longest;
-		
+
 		if count > 40
 			break
 		end
@@ -135,48 +138,48 @@ function [ret_distance, ret_route, best_round, shortest_tab] = main(filename, ty
 			% plot( f:t, shortest_tab(f:t), "g");
 			% plot( f:t, longest_tab(f:t), "r");
 			% drawnow;
-			
+
 			% offset = 1;
 		% end
-		
+
 		if headless == 0
 			nazwa_pliku = "";
 			if mod(round, 1) == 0
 				clf;
 				plot_stink(s, c(:,2:3));
 				drawnow;
-				
+
 				nazwa_pliku = sprintf("%s/%d.png", nazwa_folderu, round);
 				print(nazwa_pliku);
 				replot;
 			end;
 		end
-		
-	end	
-	
+
+	end
+
 	ret_distance = shortest;
-	
+
 	if headless == 0
 		plot_best_route(ret_route, c(:,2:3));
-	
+
 		nazwa_pliku = sprintf("%s/best.png", nazwa_folderu);
 		print(nazwa_pliku);
 		replot;
-	
+
 		figure;
 		plot(shortest_tab, "g");
 		hold on;
 		plot(longest_tab, "r");
 	end
-	
+
 	fprintf(2, "\n%d rounds in %f seconds\n\n", round, total_time);
-	
+
 	cd(nazwa_folderu);
-	
+
 	save('total.txt', 'ret_route', 'shortest_tab', 'longest_tab', 'total_time', 'round');
 	cd("../..");
-	
-	
+
+
 end
 
 % test_16.txt
